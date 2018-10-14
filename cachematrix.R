24fc82ec6695ca4913f6111d13a6373145140c08
Grayson <grayson2408@gmail.com>
2018-10-15 00:26:02 +0700

    Solve the Cache Matrix Assignment Cousera R Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This all functions will calculate Inverse Matrix and cache it
 
-## Write a short comment describing this function
+### Example Functions do:
+# mat <- matrix(c(3, 8, 4 ,6), nrow=2, ncol=2)
+# myMatrix <- makeCacheMatrix(mat)
+# cacheSolve(myMatrix)
 
+
+## makeCacheMatrix: This function creates a special "matrix" object
+## that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        # Initialize Inverse Matrix = null
+        invM <- NULL
+        
+        # Set Matrix Value (Setter)
+        set <- function(y) {
+            x <<- y
+            invM <<- NULL
+        }
+    
+        # Get Matrix Value (Getter)
+        get <- function() x
+        
+        # Set Inverse Matrix Value (Setter Inverse)
+        setinv <- function(inv) invM <<- inv
+        
+        # Get Inverse Matrix Value (Getter Inverse)
+        getinv <- function() invM
+        
+        # Set function to be accessible easier by returning as list
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+
+## cacheSolve: This function computes the inverse of the special "matrix"
+## returned by makeCacheMatrix above. If the inverse has already been
+## calculated (and the matrix has not changed), then the cachesolve
+## should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #Get Inverse Matrix
+        invM <- x$getinv()
+        
+        ## If Inverse Matrix already exist, then return cached data
+        if(!is.null(invM)){
+            message("getting cached data")
+            return(invM)
+        }
+        
+        ## If Inverse Matrix not exist, then calculate & set the Inverse Matrix
+        # Get Matrix
+        data <- x$get()
+        
+        # Calculate Inverse Matrix
+        invM <- solve(data)
+    
+        # Set Inverse Matrix
+        x$setinv(invM)
+        
+        # Return Iverse Matrix
+        invM
 }
